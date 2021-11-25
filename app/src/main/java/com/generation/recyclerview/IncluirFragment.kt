f090a5ae0c1062859152e8d3a79ceb8c901a6732
Matheus Cortez <matheus.cortez@live.com>
2021-11-25 01:16:08 -0300

    fix:tela list

--- app/src/main/java/com/generation/recyclerview/IncluirFragment.kt
+++ app/src/main/java/com/generation/recyclerview/IncluirFragment.kt
@@ -5,27 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.generation.recyclerview.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [IncluirFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class IncluirFragment : Fragment() {
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_incluir, container, false)
+        val view:View=inflater.inflate(R.layout.fragment_incluir, container, false)
+
+
+
+
+        return view
     }
+
 
 
 }
